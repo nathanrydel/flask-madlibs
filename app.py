6e66bba456c8f65c34e0c7b4ee0cac902f705256
Nathan Rydel <nathanrydel@gmail.com>
2023-11-29 16:44:22 -0700

    added app.py scaffolding and updated questions.html formatting

--- app.py
+++ app.py
@@ -7,3 +7,25 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+# TODO: create a route for the homepage that shows a form asking for madlibs
+# DO NOT hardcode the form asking these exact questions
+
+
+@app.get("/")
+def index():
+    """Render homepage form"""
+
+    html = render_template("questions.html")
+    print("html is: ", html)
+
+    return html
+
+
+# TODO: Add a route, /results, that shows the resulting story for those answers
+@app.get("/results")
+def show_results():
+    # TODO: add docstring
+    """"""
+
+    ...
