6e9efa5aa6e99d15bb56973a47d21668ef9702c6
Nathan Rydel <nathanrydel@gmail.com>
2023-11-29 18:26:36 -0700

    updates following CR

--- app.py
+++ app.py
@@ -18,13 +18,12 @@ def show_questions():
     # TypeError: Story object is not subscriptable
     # prompts = silly_story["prompts"]
 
-    return render_template("questions.html",
-                           prompts=prompts)
+    return render_template("questions.html", prompts=prompts)
 
 
 @app.get("/results")
 def show_results():
-    """Render complete story on the results endpoint"""
+    """Render complete story"""
 
     # use the get_result_text to complete the story with the input
     complete_story = silly_story.get_result_text(request.args)
